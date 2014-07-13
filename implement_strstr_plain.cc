4c9b8d48ea83e26fbba7eeda188fd5beb1a70faf
heLomaN <caozhk@gmail.com>
2014-07-14 00:02:52 +0800

    Added Implement strStr with plain method.

---
+++ implement_strstr_plain.cc
@@ -0,0 +1,25 @@
+class Solution
+{
+public:
+  char *strStr (char *haystack, char *needle)
+  {
+        char *p = NULL;
+        int lh = strlen(haystack);
+        int ln = strlen(needle);
+
+        if(lh == 0 || ln ==0)
+            return p;
+        for(int i = 0;i < lh-ln+1;i++){
+            int j = 0;
+            for(;j < ln && haystack[i+j] == needle[j];j++){
+                ;
+            }
+            if(j == ln-1){
+                p = haystack+i;
+                return p;
+            }
+        }
+        return p;
+                
+  }
+};
